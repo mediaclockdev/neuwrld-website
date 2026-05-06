86e3a28047121147009f506c0b5e6ba468e0e578
Harsh Mehta <mediaclockdev@gmail.com>
2026-05-06 10:02:39 +0530

    refresh error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect } from "react";
 import Router from "./Router/Router";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUserProfile } from "./features/auth/authSlice";
 import { fetchCategoriesAPI } from "./features/categories/categoriesSlice";
+import { fetchWishlistAPI } from "./features/wishlist/wishlistSlice";
+import { fetchCartAPI } from "./features/cart/cartSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) {
+    if (token || isLoggedIn) {
       dispatch(fetchUserProfile());
+      dispatch(fetchWishlistAPI());
+      dispatch(fetchCartAPI());
     }
-  }, []);
+  }, [dispatch, isLoggedIn]);
+
   useEffect(() => {
     dispatch(fetchCategoriesAPI());
   }, [dispatch]);
